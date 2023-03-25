ec216d4b1c1378d5eb700bdfc6298dc8b1ec2941
norulshahlam <norulshahlam@gmail.com>
2023-03-26 05:30:40 +0800

    refactor

--- src/test/java/com/shah/bankingapplicationcrud/ApplicationTests.java
+++ src/test/java/com/shah/bankingapplicationcrud/ApplicationTests.java
@@ -1,13 +1,8 @@
 package com.shah.bankingapplicationcrud;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BankingApplicationCrudApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
